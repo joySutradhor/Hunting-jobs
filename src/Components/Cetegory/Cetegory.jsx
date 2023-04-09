b4b7a063db99d58242276df47037c483bc2937a4
joySutradhor <joysutradhorcmt@gmail.com>
2023-04-10 00:57:16 +0600

    category list is completed

--- src/Components/Cetegory/Cetegory.jsx
+++ src/Components/Cetegory/Cetegory.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import CetegoryItem from './CetegoryItem';
 
 const Cetegory = () => {
     return (
         <div className='mx-20 my-10'>
-            <div className='text-center'>
+            <div className='text-center my-10'>
             <h1 className='text-2xl font-bold'>Job Category List </h1>
             <small>Explore thousands of job opportunities with all the information you need. Its your future</small>
             </div>
-            
+            <CetegoryItem></CetegoryItem>
         </div>
     );
 };
