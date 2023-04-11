2d35209d142017341f3903c583f421204cdee20c
joySutradhor <joysutradhorcmt@gmail.com>
2023-04-12 03:38:13 +0600

    blog section completed

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -5,13 +5,13 @@ const Blog = () => {
         <div className='md:mx-20'>
             <div className='shadow-sm p-2 my-3'>
                 <h1 className='font-semibold'>When should you use context API?</h1>
-                <p className='text-[#757575]'>Context API gives us super power , we can't need to props drilling . We use Context api for data passing grand father to grand child more easy way . The Context API is the React feature used for solving props drilling problems.</p>
+                <p className='text-[#757575]'>Context API gives us super power , we can't need to props drilling . We use Context api for data passing grand father to grand child more easy way . The Context API is the React feature used for solving props drilling problems.Its give us more easy way to data passing .</p>
             </div>
             <div className='shadow-sm p-2'>
                 <h1 className='font-semibold'>What is a custom hook?</h1>
                 <p className='text-[#757575]'>We can create our own custom hooks using the 'use' prefix.We are created using use prefix like as ureEffect , useSate and others .Example , i need fetch data from api , so i can use my custom hook like 'useDataFromApi' . It contains URL and return data form api .</p>
             </div>
-            <div className='shadow-sm p-2'>
+            <div className='shadow-sm p-2 my-3'>
                 <h1 className='font-semibold'>What is useRef?</h1>
                 <p className='text-[#757575]'>useRef is a hook that returns a mutable ref object.It means we can change and update the value.The useRef hook is called with an initial value as its argument, and it returns an object with a single property called current. The current property is initially set to the initial value passed to useRef, but it can be updated by assigning a new value to it.</p>
             </div>
--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -6,7 +6,6 @@ const Error = () => {
         <div>
             <div className='flex flex-col justify-center items-center h-screen'>
                 <h1 className='text-[100px] font-bold'>404 - <span className='text-red-500'>Error</span></h1>
-                {/* <Link to="/"> <button className='bg-blue-500 text-white p-2 ml-96 text-xl rounded-md'>Go back</button></Link> */}
             </div>
         </div>
     );
